c4c7a7177f0e3b08598c2bb0fd813d4bd7259e32
JakeShapiro1 <jts3681@rit.edu>
2025-09-26 09:51:54 -0400

    fixed tipping bug, rearranged player hierarchy

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // input variables
+    [HideInInspector]
     public bool isMoving;
+    [HideInInspector]
     public bool isTurningRight;
+    [HideInInspector]
     public bool isTurningLeft;
 
-    // moving variables
+    // Move Settings
+    [Header("Move Settings")]
+
+    [SerializeField]
     private float speed = 10.0f;
+
+    [SerializeField]
+    private float turningDirection = 90.0f;
+
     private Vector3 velocity;
     private Quaternion turning;
-    private float turningDirection = 90.0f;
+
+
+    // Physics Settings
+    [SerializeField]
+    private float gravityForce = 10.0f;
+
+    private Rigidbody playerBody;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        // grab rigidbody from player
+        playerBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -29,6 +46,10 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // apply artificial gravity
+        Vector3 gravity = -transform.up * gravityForce;
+        playerBody.AddForce(gravity, ForceMode.Force);
+
         // turn right if input is received
         if (isTurningRight)
         {
