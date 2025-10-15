75bcdb941406b349ece0b234b2530f0a42ff7a2e
JakeShapiro1 <jts3681@rit.edu>
2025-10-15 19:22:35 -0400

    created text on the screen representing the current day

--- Assets/Scripts/Managers/DayManager.cs
+++ Assets/Scripts/Managers/DayManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DayManager : MonoBehaviour
@@ -24,6 +26,10 @@ public class DayManager : MonoBehaviour
 
     [SerializeField]
     private Inventory inventory;
+
+    private TextMeshProUGUI text;
+
+    private float timer = 0;
 
     private void Awake()
     {
@@ -67,6 +73,10 @@ public class DayManager : MonoBehaviour
             }
         }
 
+        // find the text object attached to the player
+        Transform textObject = player.transform.Find("Canvas").Find("DayText");
+        text = textObject.GetComponent<TextMeshProUGUI>();
+
         //load in the correct data into the game
         GameData data = SaveSystem.LoadGameData();
 
@@ -92,6 +102,32 @@ public class DayManager : MonoBehaviour
             Debug.Log("No save file found, starting fresh.");
         }
 
+        ChangeDayText();
+    }
+
+    private void Update()
+    {
+        // increment timer
+        timer += Time.deltaTime;
+
+        // change the position and size of the day text for the first 1.5 seconds 
+        // after spawning in or changing days
+        if (timer < 2.0f)
+        {
+            StartCoroutine(MoveToTarget(1.5f, new Vector3(0, 0, 0)));
+            StartCoroutine(Scale(1.5f, new Vector3(2, 2, 2)));
+        }
+        else // return to original position and scale
+        {
+            // get tranform of text object
+            RectTransform textTransform = text.GetComponent<RectTransform>();
+
+            // change text position
+            textTransform.localPosition = new Vector3(-843, 491, 0);
+
+            // change text size
+            textTransform.localScale = new Vector3(1, 1, 1);
+        }
     }
 
     /// <summary>
@@ -99,6 +135,7 @@ public class DayManager : MonoBehaviour
     /// </summary>
     public void NextDay()
     {
+        timer = 0;
         currentDay++;
 
         if (currentDay > maxDays)
@@ -124,6 +161,68 @@ public class DayManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// changes the day text to represent the current day
+    /// </summary>
+    private void ChangeDayText()
+    {
+        if (text == null)
+        {
+            Debug.Log("Text Object is Null.");
+            return;
+        }
+
+        text.text = "Day: " + currentDay;
+    }
+
+    /// <summary>
+    /// Lerps the day text to the middle of the screen for a visual effect
+    /// </summary>
+    /// <param name="duration">time effect should last</param>
+    /// <param name="targetPos">position text should move to</param>
+    /// <returns></returns>
+    private IEnumerator MoveToTarget(float duration, Vector3 targetPos)
+    {
+        RectTransform textTransform = text.GetComponent<RectTransform>();
+        Vector3 start = textTransform.localPosition;
+
+        float elapsed = 0;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration;
+            textTransform.localPosition = Vector3.Lerp(start, targetPos, t);
+            yield return null;
+        }
+
+        textTransform.localPosition = targetPos;
+        ChangeDayText();
+    }
+
+    /// <summary>
+    /// Lerps the day text size bigger to create a visual effect
+    /// </summary>
+    /// <param name="duration">how long effect should last</param>
+    /// <param name="targetScale">how big text should get</param>
+    /// <returns></returns>
+    private IEnumerator Scale(float duration, Vector3 targetScale)
+    {
+        RectTransform textTransform = text.GetComponent<RectTransform>();
+        Vector3 startScale = textTransform.localScale;
+
+        float elapsed = 0;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration;
+            textTransform.localScale = Vector3.Lerp(startScale, targetScale, t);
+            yield return null;
+        }
+
+        textTransform.localScale = targetScale;
+    }
 
     /// <summary>
     /// loads in all of the items that got saved
