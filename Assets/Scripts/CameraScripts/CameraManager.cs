cf6cb99944d95ddee6ec440ffa286635f65232fe
Carter Irish <2023c.irish@gmail.com>
2025-10-15 19:09:57 -0400

    Moved CameraFollow.cs script from Jake into the dev branch and integrated it into the pre-existing camera profile structure. Also, fixed rat stutter bug caused by Update vs FixedUpdate

--- Assets/Scripts/CameraScripts/CameraManager.cs
+++ Assets/Scripts/CameraScripts/CameraManager.cs
@@ -10,9 +10,13 @@ public class CameraManager : MonoBehaviour
     public ThirdPersonCamera thirdPersonCam;
     public FirstPersonCamera firstPersonCam;
     public FixedCamera fixedCam;
+    public CameraFollow followCam;
 
     [Header("Camera Settings")]
     public CameraMode startingMode = CameraMode.ThirdPerson;
+
+    [Header("Hierarchy Management")]
+    [SerializeField] private Transform playerTransform;
 
     [SerializeField]
     private CameraMode currentMode;
@@ -21,17 +25,30 @@ public class CameraManager : MonoBehaviour
     {
         ThirdPerson,
         FirstPerson,
-        Fixed
+        Fixed,
+        Follow
     }
 
     private void Start()
     {
         // Check if assigned, if not grab automatically
         if (thirdPersonCam == null) thirdPersonCam = GetComponent<ThirdPersonCamera>();
-       
+
         if(firstPersonCam == null) firstPersonCam = GetComponent<FirstPersonCamera>();
-        
+
         if(fixedCam == null) fixedCam = GetComponent<FixedCamera>();
+
+        if(followCam == null) followCam = GetComponent<CameraFollow>();
+
+        // Auto-find player if not assigned
+        if (playerTransform == null)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+            {
+                playerTransform = player.transform;
+            }
+        }
 
         // Initalize starting cam profile
         SetCameraMode(startingMode);
@@ -52,22 +69,47 @@ public class CameraManager : MonoBehaviour
     /// <param name="newMode">The new mode.</param>
     private void SetCameraMode(CameraMode newMode)
     {
+        // Handle hierarchy changes based on camera mode
+        if (newMode == CameraMode.Follow)
+        {
+            // Follow mode: camera needs to be unparented (sibling to player in scene)
+            transform.SetParent(null);
+        }
+        else
+        {
+            // Other modes: camera needs to be child of player
+            if (playerTransform != null && transform.parent != playerTransform)
+            {
+                transform.SetParent(playerTransform);
+            }
+        }
+
+        // Enable/disable appropriate camera profile
         switch (newMode)
         {
             case CameraMode.ThirdPerson:
                 fixedCam.enabled = false;
                 firstPersonCam.enabled = false;
+                followCam.enabled = false;
                 thirdPersonCam.enabled = true;
                 break;
             case CameraMode.FirstPerson:
                 fixedCam.enabled = false;
                 firstPersonCam.enabled = true;
+                followCam.enabled = false;
                 thirdPersonCam.enabled = false;
                 break;
             case CameraMode.Fixed:
                 fixedCam.enabled = true;
                 firstPersonCam.enabled = false;
+                followCam.enabled = false;
                 thirdPersonCam.enabled = false;
+                break;
+            case CameraMode.Follow:
+                fixedCam.enabled = false;
+                firstPersonCam.enabled = false;
+                thirdPersonCam.enabled = false;
+                followCam.enabled = true;
                 break;
         }
 
@@ -79,7 +121,7 @@ public class CameraManager : MonoBehaviour
     /// </summary>
     private void CycleCameraMode()
     {
-        currentMode = (CameraMode)(((int)currentMode + 1) % 3);
+        currentMode = (CameraMode)(((int)currentMode + 1) % 4);
         SetCameraMode(currentMode);
     }
 }
